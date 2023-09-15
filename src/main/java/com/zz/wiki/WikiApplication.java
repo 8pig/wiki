97cec85a6f88c1411125a72bb21ef94897b375a8
Ezio <zhou773@foxmail.com>
2023-09-15 23:59:02 +0800

    feat: 启动文案

--- src/main/java/com/zz/wiki/WikiApplication.java
+++ src/main/java/com/zz/wiki/WikiApplication.java
@@ -1,13 +1,22 @@
 package com.zz.wiki;
 
+import org.apache.juli.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
-
+	private static final Logger LOG = LoggerFactory.getLogger(com.zz.wiki.WikiApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		String port = env.getProperty("server.port");
+		LOG.info("success:" + port);
+
+
 	}
 
 }
