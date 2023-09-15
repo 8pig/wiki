42d26d929cdfeaa33616d78ba36b99cbf5d46c2b
Ezio <zhou773@foxmail.com>
2023-09-16 00:13:38 +0800

    feat: app config move

--- src/main/java/com/zz/wiki/config/WikiApplication.java
+++ src/main/java/com/zz/wiki/config/WikiApplication.java
@@ -1,15 +1,16 @@
-package com.zz.wiki;
+package com.zz.wiki.config;
 
-import org.apache.juli.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan({"com.zz.wiki"})
 @SpringBootApplication
 public class WikiApplication {
-	private static final Logger LOG = LoggerFactory.getLogger(com.zz.wiki.WikiApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(WikiApplication.class);
 		Environment env = app.run(args).getEnvironment();
--- src/main/java/com/zz/wiki/controller/HelloController.java
+++ src/main/java/com/zz/wiki/controller/HelloController.java
@@ -1,15 +1,16 @@
-package com.zz.wiki;
+package com.zz.wiki.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
 public class HelloController {
 
-    @GetMapping("")
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
     public String get () {
 
         return  "hello";
