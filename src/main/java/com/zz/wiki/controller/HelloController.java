450223abaf4764dd7848f53c6e146324fcc1faf6
Ezio <zhou773@foxmail.com>
2023-09-16 16:08:54 +0800

    feat: 配置项

--- src/main/java/com/zz/wiki/controller/HelloController.java
+++ src/main/java/com/zz/wiki/controller/HelloController.java
@@ -1,18 +1,21 @@
 package com.zz.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+
+    @Value("${test.hello:CATCH}")
+    private String name;
+
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @GetMapping("/hello")
     public String get () {
 
-        return  "hello";
+        return  name;
     }
 }
