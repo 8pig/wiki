3d974daf0a3c32d233e14ccde3c56c3b144b5ba9
Ezio <zhou773@foxmail.com>
2023-11-05 22:28:59 +0800

    feat: 分类crud 模块

---
+++ server/src/main/java/com/zz/wiki/controller/CategoryController.java
@@ -0,0 +1,54 @@
+package com.zz.wiki.controller;
+
+
+import com.zz.wiki.req.CategoryQueryReq;
+import com.zz.wiki.req.CategorySaveReq;
+import com.zz.wiki.req.EbookQueryReq;
+import com.zz.wiki.req.EbookSaveReq;
+import com.zz.wiki.resp.CategoryQueryResp;
+import com.zz.wiki.resp.CommonResp;
+import com.zz.wiki.resp.EbookQueryResp;
+import com.zz.wiki.resp.PageResp;
+import com.zz.wiki.service.CategoryService;
+import com.zz.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+
+
+    @Resource
+    private CategoryService categoryService;
+
+    @GetMapping("/list")
+    public CommonResp<PageResp<CategoryQueryResp>> list (@Valid CategoryQueryReq req){
+        System.out.println(req.toString());
+        PageResp<CategoryQueryResp> list = categoryService.list(req);
+
+        return  CommonResp.ok(list);
+    }
+
+    @GetMapping("/all")
+    public CommonResp<List<CategoryQueryResp>> all (){
+        System.out.println(req.toString());
+        List<CategoryQueryResp> list = categoryService.all(req);
+
+        return  CommonResp.ok(list);
+    }
+
+    @PostMapping("/save")
+    public CommonResp save (@RequestBody CategorySaveReq req){
+        return  CommonResp.ok(categoryService.save(req));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete (@PathVariable Long id){
+        return  CommonResp.ok(categoryService.delete(id));
+    }
+}
---
+++ server/src/main/java/com/zz/wiki/service/CategoryService.java
@@ -0,0 +1,88 @@
+package com.zz.wiki.service;
+
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zz.wiki.domain.Category;
+import com.zz.wiki.domain.CategoryExample;
+import com.zz.wiki.mapper.CategoryMapper;
+import com.zz.wiki.req.CategoryQueryReq;
+import com.zz.wiki.req.CategorySaveReq;
+import com.zz.wiki.resp.CategoryQueryResp;
+import com.zz.wiki.resp.PageResp;
+import com.zz.wiki.util.CopyUtil;
+import com.zz.wiki.util.SnowFlake;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<CategoryQueryResp> list (CategoryQueryReq req) {
+
+        CategoryExample categoryExample = new CategoryExample();
+        // @ 相当于where 条件
+        CategoryExample.Criteria criteria = categoryExample.createCriteria();
+        categoryExample.setOrderByClause("sort asc");
+
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Category> categorys = categoryMapper.selectByExample(categoryExample);
+
+        PageInfo<Category> pageInfo = new PageInfo<>(categorys);
+        long total = pageInfo.getTotal();
+        int pages = pageInfo.getPages();
+        System.out.println(total);
+        System.out.println(pages);
+        PageResp<CategoryQueryResp> pageResp = new PageResp<>();
+        pageResp.setList(CopyUtil.copyList(categorys, CategoryQueryResp.class));
+        pageResp.setTotal(pageInfo.getTotal());
+
+
+
+        return pageResp;
+    }
+
+
+    public List<CategoryQueryResp> all (CategoryQueryReq req) {
+
+        CategoryExample categoryExample = new CategoryExample();
+        categoryExample.setOrderByClause("sort asc");
+
+
+        List<Category> categorys = categoryMapper.selectByExample(categoryExample);
+
+        List<CategoryQueryResp> list = CopyUtil.copyList(categorys, CategoryQueryResp.class);
+
+        return list;
+    }
+
+    public int save(CategorySaveReq req) {
+        /*
+         * 保存
+         * */
+        Category category = CopyUtil.copy(req, Category.class);
+        if(ObjectUtils.isEmpty(category.getId())){
+            category.setId(snowFlake.nextId());
+            return categoryMapper.insert(category);
+        }else{
+            return categoryMapper.updateByPrimaryKey(category);
+        }
+
+
+
+    }
+
+
+    public int delete(Long id) {
+        return categoryMapper.deleteByPrimaryKey(id);
+    }
+}
