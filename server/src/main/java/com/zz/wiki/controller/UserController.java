399c436a8f60695a8b69283a1f1fe2f3717c0c92
Ezio <zhou773@foxmail.com>
2023-11-18 22:01:47 +0800

    feat: 生成token  放入 redis

--- server/src/main/java/com/zz/wiki/controller/UserController.java
+++ server/src/main/java/com/zz/wiki/controller/UserController.java
@@ -9,11 +9,14 @@ import com.zz.wiki.resp.UserLoginResp;
 import com.zz.wiki.resp.UserQueryResp;
 import com.zz.wiki.resp.PageResp;
 import com.zz.wiki.service.UserService;
+import com.zz.wiki.util.SnowFlake;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +26,14 @@ public class UserController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+
 
     @GetMapping("/list")
     public CommonResp<PageResp<UserQueryResp>> list (@Valid UserQueryReq req){
@@ -51,6 +62,10 @@ public class UserController {
 
         UserLoginResp login = userService.login(req);
 
+        Long token = snowFlake.nextId();
+
+        redisTemplate.opsForValue().set(token, login, 3600 * 24, TimeUnit.SECONDS);
+        login.setToken(token.toString());
 
 
         return   CommonResp.ok(login);
